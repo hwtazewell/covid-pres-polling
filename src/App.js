2f942e55751d3effb401a45dfd8373af06fe4d4f
Henry Tazewell <hwtazewell@email.wm.edu>
2020-10-29 23:48:01 -0400

    removed select, added clickable map

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import React, { Component } from 'react';
 import axios from 'axios';
 import { CanvasJSChart } from 'canvasjs-react-charts';
-import Select from 'react-select';
-import { Container } from 'reactstrap';
 import _ from 'lodash';
+import USAMap from "react-usa-map";
 
 const states = [
   'Alabama',
@@ -59,12 +58,66 @@ const states = [
   'Wyoming'
 ]
 
+const abbrs = [
+'AL',
+'AK',
+'AZ',
+'AR',
+'CA',
+'CO',
+'CT',
+'DE',
+'FL',
+'GA',
+'HI',
+'ID',
+'IL',
+'IN',
+'IA',
+'KS',
+'KY',
+'LA',
+'ME',
+'MD',
+'MA',
+'MI',
+'MN',
+'MS',
+'MO',
+'MT',
+'NE',
+'NV',
+'NH',
+'NJ',
+'NM',
+'NY',
+'NC',
+'ND',
+'OH',
+'OK',
+'OR',
+'PA',
+'RI',
+'SC',
+'SD',
+'TN',
+'TX',
+'UT',
+'VT',
+'VA',
+'WA',
+'WV',
+'WI',
+'WY'
+]
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       covid_data: [],
       state: null,
+      abbr: null,
       polling_data: [],
       covid_data_by_state: [],
       polling_data_by_state: [],
@@ -133,6 +186,174 @@ class App extends Component {
       }
     };
   }
+
+  mapHandler = (event) => {
+    let state_index = _.indexOf(abbrs, event.target.dataset.name)
+    this.setState({
+      state: states[state_index],
+      abbr: event.target.dataset.name
+    }, this.selectData(states[state_index]))
+    let highlight = {}
+    highlight[event.target.dataset.name] = {
+      fill: "#CC0000"
+    }
+    this.statesCustomConfig(highlight)
+  };
+
+  statesCustomConfig = () => {
+    return {
+      'AL': {
+        fill: this.state.abbr === 'AL' ? "navy" : null
+      },
+      'AK': {
+        fill: this.state.abbr === 'AK' ? "navy" : null
+      },
+      'AZ': {
+        fill: this.state.abbr === 'AZ' ? "navy" : null
+      },
+      'AR': {
+        fill: this.state.abbr === 'AR' ? "navy" : null
+      },
+      'CA': {
+        fill: this.state.abbr === 'CA' ? "navy" : null
+      },
+      'CO': {
+        fill: this.state.abbr === 'CO' ? "navy" : null
+      },
+      'CT': {
+        fill: this.state.abbr === 'CT' ? "navy" : null
+      },
+      'DE': {
+        fill: this.state.abbr === 'DE' ? "navy" : null
+      },
+      'FL': {
+        fill: this.state.abbr === 'FL' ? "navy" : null
+      },
+      'GA': {
+        fill: this.state.abbr === 'GA' ? "navy" : null
+      },
+      'HI': {
+        fill: this.state.abbr === 'HI' ? "navy" : null
+      },
+      'ID': {
+        fill: this.state.abbr === 'ID' ? "navy" : null
+      },
+      'IL': {
+        fill: this.state.abbr === 'IL' ? "navy" : null
+      },
+      'IN': {
+        fill: this.state.abbr === 'IN' ? "navy" : null
+      },
+      'IA': {
+        fill: this.state.abbr === 'IA' ? "navy" : null
+      },
+      'KS': {
+        fill: this.state.abbr === 'KS' ? "navy" : null
+      },
+      'KY': {
+        fill: this.state.abbr === 'KY' ? "navy" : null
+      },
+      'LA': {
+        fill: this.state.abbr === 'LA' ? "navy" : null
+      },
+      'ME': {
+        fill: this.state.abbr === 'ME' ? "navy" : null
+      },
+      'MD': {
+        fill: this.state.abbr === 'MD' ? "navy" : null
+      },
+      'MA': {
+        fill: this.state.abbr === 'MA' ? "navy" : null
+      },
+      'MI': {
+        fill: this.state.abbr === 'MI' ? "navy" : null
+      },
+      'MN': {
+        fill: this.state.abbr === 'MN' ? "navy" : null
+      },
+      'MS': {
+        fill: this.state.abbr === 'MS' ? "navy" : null
+      },
+      'MO': {
+        fill: this.state.abbr === 'MO' ? "navy" : null
+      },
+      'MT': {
+        fill: this.state.abbr === 'MT' ? "navy" : null
+      },
+      'NE': {
+        fill: this.state.abbr === 'NE' ? "navy" : null
+      },
+      'NV': {
+        fill: this.state.abbr === 'NV' ? "navy" : null
+      },
+      'NH': {
+        fill: this.state.abbr === 'NH' ? "navy" : null
+      },
+      'NJ': {
+        fill: this.state.abbr === 'NJ' ? "navy" : null
+      },
+      'NM': {
+        fill: this.state.abbr === 'NM' ? "navy" : null
+      },
+      'NY': {
+        fill: this.state.abbr === 'NY' ? "navy" : null
+      },
+      'NC': {
+        fill: this.state.abbr === 'NC' ? "navy" : null
+      },
+      'ND': {
+        fill: this.state.abbr === 'ND' ? "navy" : null
+      },
+      'OH': {
+        fill: this.state.abbr === 'OH' ? "navy" : null
+      },
+      'OK': {
+        fill: this.state.abbr === 'OK' ? "navy" : null
+      },
+      'OR': {
+        fill: this.state.abbr === 'OR' ? "navy" : null
+      },
+      'PA': {
+        fill: this.state.abbr === 'PA' ? "navy" : null
+      },
+      'RI': {
+        fill: this.state.abbr === 'RI' ? "navy" : null
+      },
+      'SC': {
+        fill: this.state.abbr === 'SC' ? "navy" : null
+      },
+      'SD': {
+        fill: this.state.abbr === 'SD' ? "navy" : null
+      },
+      'TN': {
+        fill: this.state.abbr === 'TN' ? "navy" : null
+      },
+      'TX': {
+        fill: this.state.abbr === 'TX' ? "navy" : null
+      },
+      'UT': {
+        fill: this.state.abbr === 'UT' ? "navy" : null
+      },
+      'VT': {
+        fill: this.state.abbr === 'VT' ? "navy" : null
+      },
+      'VA': {
+        fill: this.state.abbr === 'VA' ? "navy" : null
+      },
+      'WA': {
+        fill: this.state.abbr === 'WA' ? "navy" : null
+      },
+      'WV': {
+        fill: this.state.abbr === 'WV' ? "navy" : null
+      },
+      'WI': {
+        fill: this.state.abbr === 'WI' ? "navy" : null
+      },
+      'WY': {
+        fill: this.state.abbr === 'WY' ? "navy" : null
+      },
+    };
+  };
 
   componentDidMount() {
     this.getCovidData()
@@ -260,7 +481,10 @@ class App extends Component {
       ...this.state,
       options: {
         ...this.state.options,
-        data: data
+        data: data,
+        title: {
+          text: "Presidential Polls vs Covid Deaths for " + state
+        }
       }
     })
   }
@@ -268,10 +492,8 @@ class App extends Component {
   render() {
     return (
       <div style={{width: "1000px", margin: 'auto', paddingTop: '50px'}}>
-        <Container fluid>
-          <CanvasJSChart options = {this.state.options} onRef={ref => this.chart = ref} />
-          <Select options={states.map(x => { return {label: x, value: x} })} onChange={(state) => this.setState({ state: state.value }, this.selectData(state.value))}/>          
-        </Container>
+        <CanvasJSChart options = {this.state.options} onRef={ref => this.chart = ref} />
+        <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
       </div>
     );
   }
